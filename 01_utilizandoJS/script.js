3cf14ff1ddc74d82ca150416aa2b00a287d96b7d
EricBier <ericfrancax96@gmail.com>
2021-01-21 10:00:39 -0300

    Iniciando aulas de JavaScript, com Tio Mazinho.

---
+++ 01_utilizandoJS/script.js
@@ -0,0 +1,17 @@
+function mostrarAlert() {
+    alert('Na tag_alert')
+}
+
+function mostrarConfirm()
+{
+    confirm('Na tag_Confirm')
+}
+
+function iniciarPormpt()
+{
+    prompt('Qual o seu nome')
+}
+
+// function nomeFuncao() { 
+//     //escopo da função //
+// }
